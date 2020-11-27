42d62e31d16e6f0ec4c82952d05bf07caa2fb6c6
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-26 22:11:14 -0300

    Hello React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Teste';
+const element = <h1>Olá, {name} </h1>
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Olá, {name} </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
