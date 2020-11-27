06508e13d075f48d9cf5dfe7c8edf7b6a025a278
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-26 23:06:43 -0300

    Testing component interaction

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
 
 const name = 'Teste';
-const element = <h1>Olá, {name} </h1>
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form>
+          <div>
+            <input type='text' />
+          </div>
+        </form>
         <h1>Olá, {name} </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Game />
+        </div>
       </header>
     </div>
+
   );
 }
 
---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render() {
+        return (
+            <p> testing components comm's</p>
+        );
+      }
+    }
+
+export default Game;
